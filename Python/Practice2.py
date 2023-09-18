7a537bd1990478902e4832ff9de626fc8f14fc26
ownvdz <128668961+ownvdz@users.noreply.github.com>
2023-09-18 21:35:32 +0900

    Add files via upload

--- Python/Practice2.py
+++ Python/Practice2.py
@@ -4,4 +4,4 @@ num3 = int(input("3rd:"))
 
 Average = (num1+num2+num3)/3
 
-print(num1+","+num2+","+num3+"의 평균은 "+"%.2f"%Average+"입니다.")
+print(str(num1)+","+str(num2)+","+str(num3)+"의 평균은 "+"%.2f"%Average+"입니다.")
--- Python/Practice3.py
+++ Python/Practice3.py
@@ -1,7 +1,9 @@
 Input_Money = int(input("투입한 돈:"))
 Price = int(input("물건의 가격:"))
+
 Change = Input_Money-Price
-print("거스름 돈:",Change)
+print("거스름 돈:"+str(Change))
+
 Change_500 = Change//500
-print("500 동전 개수:",Change_500)
-print("100 동전 개수:",(Change%500)//100)
+print("500 동전 개수:"+str(Change_500))
+print("100 동전 개수:"+str((Change%500)//100))
